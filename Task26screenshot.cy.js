7c945d5890daca6a2ccd97549567b0a57bc3d279
guruprathibaqaoncloud <guruprathiba@qaoncloud.com>
2025-01-11 09:11:01 +0530

    Add files via upload

--- Task26screenshot.cy.js
+++ Task26screenshot.cy.js
@@ -1,29 +1,4 @@
-describe("Capture screenshot and Videos", () => {
-  it("Takeing screenshot using Screenshot command", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
-    cy.wait(3000);
-
-    cy.screenshot("1 Login page of OrangeHRM");
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("Admin");
-    cy.get(
-      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("admin123");
-    cy.wait(3000);
-
-    cy.screenshot("2 Login page with filled inputs");
-    cy.get(".oxd-button").click();
-    cy.wait(3000);
-    cy.screenshot("3 After Login OrangeHRM Dashboard");
-    cy.wait(3000);
-
-    cy.get(".oxd-brand-banner > img").screenshot(
-      "4 Logo of OrangeHRM Dashboard"
-    );
-  });
+describe("My first Test suite", function () {
   it("Screenshot and Videos on Testfailure", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
